f0ba2f825b4674f7bb63060dbbbef50d54f39571
Rizky Bintang <rizkybf24@gmail.com>
2026-06-10 14:16:16 +0700

    Persiapan Lab 13

---
+++ server_smartcity/smartcity_app/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for smartcity_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'smartcity_app.settings'
+)
+
+application = get_asgi_application()
---
+++ server_smartcity/smartcity_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for smartcity_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'smartcity_app.settings'
+)
+
+application = get_wsgi_application()
